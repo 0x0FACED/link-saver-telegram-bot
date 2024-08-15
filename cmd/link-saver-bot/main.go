dada28cf1238c592d3d840ba261a1ddd33df2bf2
0x0FACED <https.genius@gmail.com>
2024-08-15 17:50:02 +0300

    added: start call, mustToken func

--- cmd/link-saver-bot/main.go
+++ cmd/link-saver-bot/main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"flag"
+	"log"
+
+	"github.com/0x0FACED/link-saver-telegram-bot/internal/telegram"
+)
+
 func main() {
+	bot := telegram.New(mustToken(), "aasdasd")
+
+	bot.Start()
+}
+
+func mustToken() string {
+	token := flag.String("token", "", "token for telegram bot")
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatalln("token must be specified")
+	}
+
+	return *token
 }
