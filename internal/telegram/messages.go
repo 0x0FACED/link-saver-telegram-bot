e1b9eb4c2fcf985b3fc6f64423109d9b3a717be3
0x0FACED <https.genius@gmail.com>
2024-08-16 13:46:31 +0300

    impl: bot messages file

---
+++ internal/telegram/messages.go
@@ -0,0 +1,15 @@
+package telegram
+
+var (
+	helpMsg = `To save message, use the following syntax: 
+	<link> <name/description>
+	example: https://youtu.be/watch/ADASDASD youtube video
+	
+	To get link, use the following syntax:
+	<name/description>
+	example: youtube video
+
+	To delete the link, use the followin syntax:
+	example: youtube video
+	`
+)
