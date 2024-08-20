4a43537c95771a1c818f6b2e17f04cd164d1fe5f
0x0FACED <https.genius@gmail.com>
2024-08-20 14:11:58 +0300

    impl: `SaveLink` method

--- internal/grpc/grpc.go
+++ internal/grpc/grpc.go
@@ -5,26 +5,37 @@ import (
 
 	"github.com/0x0FACED/proto-files/link_service/gen"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type APIClient struct {
 	client gen.LinkServiceClient
 }
 
-func New(conn *grpc.ClientConn) *APIClient {
+func New(host string) (*APIClient, error) {
+	conn, err := grpc.NewClient(host, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		return nil, err
+	}
+
 	return &APIClient{
 		client: gen.NewLinkServiceClient(conn),
+	}, nil
+}
+
+func (a APIClient) GetLinks(ctx context.Context, req *gen.GetLinksRequest) (*gen.GetLinksResponse, error) {
+	panic("TODO: impl me")
+}
+
+func (a APIClient) SaveLink(ctx context.Context, req *gen.SaveLinkRequest) (*gen.SaveLinkResponse, error) {
+	resp, err := a.client.SaveLink(ctx, req)
+	if err != nil {
+		return nil, err
 	}
+	return resp, nil
 }
 
-func (a APIClient) GetLinks(ctx context.Context, req gen.GetLinksRequest) (*gen.GetLinksResponse, error) {
-	panic("TODO: impl me")
-}
-
-func (a APIClient) SaveLink(ctx context.Context, req gen.SaveLinkRequest) (*gen.SaveLinkResponse, error) {
-	panic("TODO: impl me")
-}
-
-func (a APIClient) DeleteLink(ctx context.Context, req gen.DeleteLinkRequest) (*gen.DeleteLinkResponse, error) {
+func (a APIClient) DeleteLink(ctx context.Context, req *gen.DeleteLinkRequest) (*gen.DeleteLinkResponse, error) {
 	panic("TODO: impl me")
 }
