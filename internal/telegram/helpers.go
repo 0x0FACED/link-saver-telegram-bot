cd4f4adbfa57636efa802fe41f42e7a4cb5a6742
0x0FACED <https.genius@gmail.com>
2024-08-26 15:07:55 +0300

    upd: the logic of obtaining all user links is placed in a separate method

--- internal/telegram/helpers.go
+++ internal/telegram/helpers.go
@@ -2,7 +2,9 @@ package telegram
 
 import (
 	"context"
+	"time"
 
+	"github.com/0x0FACED/proto-files/link_service/gen"
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 	"go.uber.org/zap"
@@ -24,4 +26,28 @@ func (h *Handlers) getLinksHandlerHelper(ctx context.Context, b *bot.Bot, update
 		Text:      getMsg_EN,
 		ParseMode: models.ParseModeMarkdown,
 	})
+}
+
+func (h *Handlers) getAllLinks(ctx context.Context, b *bot.Bot, update *models.Update) []*gen.Link {
+	req := &gen.GetAllLinksRequest{
+		UserId: update.Message.From.ID,
+	}
+
+	withTimeout, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	resp, err := h.api.GetAllLinks(withTimeout, req)
+	if err != nil {
+		h.logger.Error("some error: "+err.Error(), zap.Int64("user", update.Message.From.ID), zap.String("mes", update.Message.Text))
+
+		b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID: update.Message.Chat.ID,
+			Text:   "Error! Try again." + err.Error(),
+		})
+		return nil
+	}
+
+	h.logger.Debug("got links: ", zap.Any("links", resp.Links))
+
+	return resp.Links
 }
