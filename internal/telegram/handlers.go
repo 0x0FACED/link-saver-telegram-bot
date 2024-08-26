898d08530134a25c57ffbe68eb1583a3df04c115
0x0FACED <https.genius@gmail.com>
2024-08-26 15:07:22 +0300

    added: delete link handler impl, get all links for deletion handler. refactored: get links handler,

--- internal/telegram/handlers.go
+++ internal/telegram/handlers.go
@@ -3,12 +3,12 @@ package telegram
 import (
 	"context"
 	"fmt"
-	"strconv"
 	"strings"
 	"time"
 
 	"github.com/0x0FACED/link-saver-telegram-bot/internal/grpc"
 	"github.com/0x0FACED/link-saver-telegram-bot/internal/logger/zaplog"
+	"github.com/0x0FACED/link-saver-telegram-bot/internal/utils"
 	"github.com/0x0FACED/proto-files/link_service/gen"
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
@@ -42,7 +42,7 @@ func (h *Handlers) initHandlers() {
 	h.handlers["/list"] = h.getAllLinksHandler
 	h.handlers["/save"] = h.saveLinkHandler
 	h.handlers["/delete"] = h.deleteLinkHandler
-	h.handlers["/list"] = h.getAllLinksHandler
+	h.handlers["/del"] = h.getAllLinksHandlerDelete
 }
 
 func (h *Handlers) mainHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
@@ -98,24 +98,34 @@ func (h *Handlers) saveLinkHandler(ctx context.Context, b *bot.Bot, update *mode
 	defer cancel()
 
 	h.logger.Debug("saveLinkHandler() request: ", zap.Any("req", req))
-	resp, err := h.api.SaveLink(withTimeout, req)
+	_, err := h.api.SaveLink(withTimeout, req)
 
 	if err != nil {
-		h.logger.Error("error by API: "+err.Error(),
+		msg := utils.GetCodeMsgFromError(err)
+		h.logger.Error("error by API",
 			zap.String("user", update.Message.Chat.Username),
 			zap.String("user_msg", update.Message.Text),
+			zap.Error(err),
 		)
+
+		b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID: update.Message.Chat.ID,
+			Text:   msg + " 😞",
+		})
+
+		return
 	}
 
 	b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: update.Message.Chat.ID,
-		Text:   resp.Message,
+		Text:   "Successfully Saved",
 	})
 }
 
 func (h *Handlers) deleteLinkHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
 	h.logger.Debug("deleteLinkHandler(): "+update.Message.Text, zap.Int64("user", update.Message.From.ID))
 
+	h.getAllLinksHandler(ctx, b, update)
 }
 
 func (h *Handlers) getLinksHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
@@ -151,105 +161,37 @@ func (h *Handlers) getLinksHandler(ctx context.Context, b *bot.Bot, update *mode
 
 	h.logger.Debug("got links: ", zap.Any("links", resp.Links))
 
-	h.inlineKbHandler(ctx, b, update, resp.Links)
+	h.inlineKbHandler(ctx, b, update, resp.Links, "get")
+}
+
+func (h *Handlers) getAllLinksHandlerDelete(ctx context.Context, b *bot.Bot, update *models.Update) {
+
+	h.logger.Debug("getAllLinksHandler(): "+update.Message.Text, zap.Int64("user", update.Message.From.ID))
+	links := h.getAllLinks(ctx, b, update)
+	h.inlineKbHandler(ctx, b, update, links, "del")
 }
 
 func (h *Handlers) getAllLinksHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
-
 	h.logger.Debug("getAllLinksHandler(): "+update.Message.Text, zap.Int64("user", update.Message.From.ID))
-
-	req := &gen.GetAllLinksRequest{
-		UserId: update.Message.From.ID,
-	}
-
-	withTimeout, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-	defer cancel()
-
-	resp, err := h.api.GetAllLinks(withTimeout, req)
-	if err != nil {
-		h.logger.Error("some error: "+err.Error(), zap.Int64("user", update.Message.From.ID), zap.String("mes", update.Message.Text))
-
-		b.SendMessage(ctx, &bot.SendMessageParams{
-			ChatID: update.Message.Chat.ID,
-			Text:   "Error! Try again." + err.Error(),
-		})
-		return
-	}
-
-	h.logger.Debug("got links: ", zap.Any("links", resp.Links))
-
-	h.inlineKbHandler(ctx, b, update, resp.Links)
+	links := h.getAllLinks(ctx, b, update)
+	h.inlineKbHandler(ctx, b, update, links, "get")
 }
 
-func (h *Handlers) callbackInlineKbHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
-	b.AnswerCallbackQuery(ctx, &bot.AnswerCallbackQueryParams{
-		CallbackQueryID: update.CallbackQuery.ID,
-		ShowAlert:       false,
-	})
-
-	chosen := strings.SplitN(update.CallbackQuery.Data, ":", 3)
-	if len(chosen) != 3 {
-		b.SendMessage(ctx, &bot.SendMessageParams{
-			ChatID: update.CallbackQuery.Message.Message.Chat.ID,
-			Text:   "Some error, try again.",
-		})
-		return
-	}
-
-	h.logger.Info("request GetLink params", zap.String("id", chosen[1]), zap.String("desc", chosen[2]))
-	idReq, err := strconv.Atoi(chosen[1])
-
-	if err != nil {
-		h.logger.Error("error happened", zap.Error(err))
-
-		b.SendMessage(ctx, &bot.SendMessageParams{
-			ChatID: update.Message.Chat.ID,
-			Text:   "Error! Try again." + err.Error(),
-		})
-		return
-	}
-
-	req := &gen.GetLinkRequest{
-		UrlId:       int32(idReq),
-		UserId:      update.CallbackQuery.From.ID,
-		Description: chosen[2],
-	}
-
-	withTimeout, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-	defer cancel()
-
-	resp, err := h.api.GetLink(withTimeout, req)
-	if err != nil {
-		h.logger.Error("error happened", zap.Error(err))
-
-		b.SendMessage(ctx, &bot.SendMessageParams{
-			ChatID: update.CallbackQuery.Message.Message.Chat.ID,
-			Text:   "Error! Try again." + err.Error(),
-		})
-		return
-	}
-
-	text := fmt.Sprintf("*Description:* \n_%s_\n\n*Generated link:* `%s`", chosen[2], resp.GeneratedUrl)
-	b.SendMessage(ctx, &bot.SendMessageParams{
-		ChatID:    update.CallbackQuery.Message.Message.Chat.ID,
-		Text:      text,
-		ParseMode: models.ParseModeMarkdownV1,
-	})
-}
-
-func (h *Handlers) inlineKbHandler(ctx context.Context, b *bot.Bot, update *models.Update, links []*gen.Link) {
+func (h *Handlers) inlineKbHandler(ctx context.Context, b *bot.Bot, update *models.Update, links []*gen.Link, tag string) {
 	if len(links) == 0 {
 		b.SendMessage(ctx, &bot.SendMessageParams{
 			ChatID: update.Message.Chat.ID,
 			Text:   "No saved links 😞",
 		})
+
+		return
 	}
 
 	inline := &models.InlineKeyboardMarkup{}
 	buttons := make([][]models.InlineKeyboardButton, 0, len(links))
 	for i, link := range links {
 		text := fmt.Sprintf("%d. %s: %s", i, link.Description, link.OriginalUrl)
-		callbackData := fmt.Sprintf("button:%d:%s", link.LinkId, link.Description)
+		callbackData := fmt.Sprintf("%s:%d:%s", tag, link.LinkId, link.Description)
 
 		button := models.InlineKeyboardButton{
 			Text:         text,
