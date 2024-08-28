5a2028aff6e8e375044066fa5cf7362d3c6cc297
0x0FACED <https.genius@gmail.com>
2024-08-28 13:08:37 +0300

    added: cfg load

--- cmd/link-saver-bot/main.go
+++ cmd/link-saver-bot/main.go
@@ -4,11 +4,16 @@ import (
 	"flag"
 	"log"
 
+	"github.com/0x0FACED/link-saver-telegram-bot/config"
 	"github.com/0x0FACED/link-saver-telegram-bot/internal/telegram"
 )
 
 func main() {
-	bot := telegram.New(mustToken(), "localhost:50051")
+	cfg, err := config.Load()
+	if err != nil {
+		panic("Config didn't loaded.")
+	}
+	bot := telegram.New(cfg)
 	if bot == nil {
 		panic("Bot didn't created.")
 	}
